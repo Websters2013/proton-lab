ff90475ebea7cc298a170fef21eda2ad58d6dc25
iversontb <petruninevgeniy@gmial.com>
2017-05-19 17:57:00 +0300

    done

--- gulpfile.js
+++ gulpfile.js
@@ -78,9 +78,9 @@ gulp.task('php', function () {
 gulp.task('styles', function () {
     return gulp.src(paths.styles)
         .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(sass({}).on('error', sass.logError))
         .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
-        .pipe(sourcemaps.write())
+        //.pipe(sourcemaps.write())
         .pipe(gulp.dest('dist/css'))
         .pipe(browserSync.stream());
 });
@@ -90,9 +90,9 @@ gulp.task( 'scripts', function () {
         gulp.src( paths.scripts[ i ].contains )
             .pipe(sourcemaps.init())
             // .pipe(babel({presets: ['es2015']})) //for js6
-            .pipe(uglify())
+            //.pipe(uglify())
             .pipe(concat(paths.scripts[ i ].dist))
-            .pipe(sourcemaps.write())
+            //.pipe(sourcemaps.write())
             .pipe(gulp.dest('dist/js/'));
     }
 });
